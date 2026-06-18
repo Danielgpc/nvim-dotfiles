1ddffdf7f5d2a87750809780842141e2360a8c0f
Daniel Carvalho <danielgpcarvalho20121212@gmail.com>
2026-06-18 20:05:21 -0300

    Reconfigured the entire nvim configuration with a Lazy.nvim starter based config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Mover para a janela da esquerda
+vim.keymap.set("n", "<A-h>", "<C-w>h", { desc = "Move to the window on the left" })
+
+-- Mover para a janela de baixo
+vim.keymap.set("n", "<A-j>", "<C-w>j", { desc = "Move to the window below" })
+
+-- Mover para a janela de cima
+vim.keymap.set("n", "<A-k>", "<C-w>k", { desc = "Move to the window above" })
+
+-- Mover para a janela da direita
+vim.keymap.set("n", "<A-l>", "<C-w>l", { desc = "Move to the window on the right" })
+
+-- Disable arrow keys in normal, insert, and visual modes
+local keys = { "<Up>", "<Down>", "<Left>", "<Right>" }
+for _, key in ipairs(keys) do
+  vim.keymap.set({ "n", "i", "v" }, key, "<Nop>", { noremap = true, silent = true })
+end
